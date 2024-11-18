55f95e604f018ea1236ad8f46beef80b947c0372
leandrofa1980 <leandrofa1980@gmail.com>
2024-11-17 22:04:54 -0300

    Finalizando projeto, implementando troca de posição dos games

--- src/main/java/com/leandrofa1980/DSList/controllers/GameListController.java
+++ src/main/java/com/leandrofa1980/DSList/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leandrofa1980.DSList.dto.GameListDTO;
 import com.leandrofa1980.DSList.dto.GameMinDTO;
+import com.leandrofa1980.DSList.dto.ReplacementDTO;
 import com.leandrofa1980.DSList.services.GameListService;
 import com.leandrofa1980.DSList.services.GameService;
 
@@ -33,6 +36,11 @@ public class GameListController {
 	public List<GameMinDTO> findByList(@PathVariable Long listId) {
 		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
+	}
+	
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+		service.move(listId, body.getSourceIndex(), body.getDestinationIndex());
 	}
 
 }
--- src/main/java/com/leandrofa1980/DSList/repositories/GameListRepository.java
+++ src/main/java/com/leandrofa1980/DSList/repositories/GameListRepository.java
@@ -1,9 +1,15 @@
 package com.leandrofa1980.DSList.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.leandrofa1980.DSList.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+	@Modifying // É usado quando uma consulta não é feita com SELECT usamos essa anotação 
+	@Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition); 
 
 }
